eb7a1bb96152dea1f3d34ea70a262fbf01d2df12
Boris2514 <borismiashiro@gmail.com>
2023-07-19 22:22:29 -0400

    Updating Doc

--- src/sockets_0/Cliente.java
+++ src/sockets_0/Cliente.java
@@ -3,9 +3,11 @@ package sockets_0;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
-
 
 public class Cliente {
 	public static void main(String[] args) {
@@ -14,39 +16,148 @@ public class Cliente {
 	}
 }
 
-
 class MarcoCliente extends JFrame{
 	public MarcoCliente(){
 		setBounds(600,300,280,350);
 		LaminaMarcoCliente milamina=new LaminaMarcoCliente();
 		add(milamina);
 		setVisible(true);
+
+		addWindowListener(new Online());
 		}
 }
 
-class LaminaMarcoCliente extends JPanel{
-	private JTextField campo1;
+// Envio de la ip del cliente al servidor
+class Online extends WindowAdapter {
+	 public void windowOpened(WindowEvent e) {
+		 try {
+			 Socket socket = new Socket("192.168.0.11", 9999);
+			 DataPackage packedData = new DataPackage();
+			 packedData.setMessage("Online");
+
+			 ObjectOutputStream outFlowData = new ObjectOutputStream( socket.getOutputStream() );
+			 outFlowData.writeObject(packedData);
+
+			 socket.close();
+		 } catch(IOException ex) {
+			 throw new RuntimeException(ex);
+		 }
+
+	 }
+}
+
+class LaminaMarcoCliente extends JPanel implements Runnable{
+	private String user;
+	private JTextField messagesArea;
+	private JComboBox< String > ip;
+	/*private JLabel nickName;*/
+	/*private JTextField ip;*/
 	private JButton miboton;
+	private JTextArea chatField;
 	public LaminaMarcoCliente(){
-		JLabel texto=new JLabel("CLIENTE");
-		add(texto);
-		campo1=new JTextField(20);
-		add(campo1);
-		miboton=new JButton("Enviar");
-		sendText sendText = new sendText();
+		user = JOptionPane.showInputDialog("Your Nickname: ");
+		JLabel nickLabel = new JLabel("Nick: " + user);
+		/*nickName = new JLabel();
+		nickName.setText(user);*/
+		JLabel texto = new JLabel("Online: ");
+		ip = new JComboBox<>();
+		ip.addItem("Usuario - 1");
+		ip.addItem("Usuario - 2");
+		ip.addItem("Usuario - 3");
+
+		chatField = new JTextArea(10,20);
+		messagesArea = new JTextField(20);
+		miboton = new JButton("Enviar");
+		SendText sendText = new SendText();
+
 		miboton.addActionListener(sendText);
+
+		add(nickLabel);
+		/*add(nickName);*/
+		add(texto);
+		add(ip);
+		add(chatField);
+		add(messagesArea);
 		add(miboton);
+		Thread thread = new Thread(this);
+		thread.start();
 	}
 
-	private class sendText implements ActionListener {
+	@Override
+	public void run() {
+		try {
+			ServerSocket server_client = new ServerSocket(9090);
+			Socket client;
+			DataPackage packedDataReceived;
+
+			while(true) {
+				client = server_client.accept();
+				ObjectInputStream inFlowData = new ObjectInputStream( client.getInputStream() );
+
+				packedDataReceived = ( DataPackage ) inFlowData.readObject();
+				chatField.append("\n" + packedDataReceived.getNickName() +" - " + packedDataReceived.getIp() +":\n" + packedDataReceived.getMessage());
+			}
+		} catch( IOException e ) {
+			System.out.println(e.getMessage());
+		} catch( ClassNotFoundException e ) {
+			throw new RuntimeException( e );
+		}
+	}
+
+	private class SendText implements ActionListener {
 		@Override
 		public void actionPerformed( ActionEvent event ) {
+			chatField.append( "\n" + messagesArea.getText() );
 			try {
 				Socket miSocket = new Socket("192.168.0.11", 9999);
+				DataPackage packedData = new DataPackage(user, messagesArea.getText(), ip.getSelectedItem().toString());
+				ObjectOutputStream objectOutputStream = new ObjectOutputStream(miSocket.getOutputStream());
 
+				/*objectOutputStream.writeObject(packedData);*/
+				/*DataOutputStream data_out_flow = new DataOutputStream(miSocket.getOutputStream());
+				data_out_flow.writeUTF( campo1.getText());
+
+				data_out_flow.close();*/
+				miSocket.close();
 			} catch( IOException e ) {
 				System.out.println(e.getMessage());
 			}
 		}
+	}
+}
+//"192.168.0.11"
+class DataPackage implements Serializable {
+	private String nickName;
+	private String message;
+	private String ip;
+	public DataPackage( String nickName, String message, String ip )  {
+		this.nickName = nickName;
+		this.message = message;
+		this.ip = ip;
+	}
+
+	public DataPackage() {
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public String getIp() {
+		return ip;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
 	}
 }
--- src/sockets_0/Servidor.java
+++ src/sockets_0/Servidor.java
@@ -2,6 +2,12 @@ package sockets_0;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor  {
 	public static void main(String[] args) {
@@ -10,7 +16,7 @@ public class Servidor  {
 	}	
 }
 
-class MarcoServidor extends JFrame {
+class MarcoServidor extends JFrame implements Runnable {
 	private	JTextArea areatexto;
 	public MarcoServidor(){
 		setBounds(1200,300,280,350);
@@ -20,5 +26,49 @@ class MarcoServidor extends JFrame {
 		milamina.add(areatexto,BorderLayout.CENTER);
 		add(milamina);
 		setVisible(true);
+		Thread hilo = new Thread(this);
+		hilo.start();
 		}
+
+	@Override
+	public void run() {
+		try {
+			ServerSocket server = new ServerSocket(9999);
+			String nick, message, ip, ipClientLocation;
+			DataPackage packedDataReceived;
+
+			while(true) {
+				Socket mi_socket = server.accept();
+				//Detecta la direccion ip del cliente
+				ipClientLocation = mi_socket.getInetAddress().getHostAddress();
+				System.out.println(ipClientLocation);
+				// -------------------------------
+				ObjectInputStream objectInputStream = new ObjectInputStream(mi_socket.getInputStream());
+				packedDataReceived = ( DataPackage ) objectInputStream.readObject();
+
+				nick = packedDataReceived.getNickName();
+				message = packedDataReceived.getMessage();
+				ip = packedDataReceived.getIp();
+
+				areatexto.append("\n" + nick +" - " + ip +":\n" + message);
+				/*DataInputStream data_in_flow = new DataInputStream(mi_socket.getInputStream());
+				String message_received = data_in_flow.readUTF();*/
+				/*areatexto.append("\n"+message_received);
+				data_in_flow.close();*/
+
+				Socket sendToAdressee = new Socket(ip, 9090);
+				ObjectOutputStream packageToBeSent = new ObjectOutputStream(sendToAdressee.getOutputStream());
+
+				packageToBeSent.writeObject(packedDataReceived);
+
+				packageToBeSent.close();
+				sendToAdressee.close();
+				mi_socket.close();
+			}
+		} catch( IOException | ClassNotFoundException e ) {
+			throw new RuntimeException( e );
+		}
+	}
 }
+
+
